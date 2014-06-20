327be374764da39f2464e365f8e3a94e96258fe3
Gregg Varona <greggvarona@gmail.com>
2014-06-20 11:57:29 +0800

    Created a filter for gray scaling.

--- src/org/sanwrit/tdect/io/ImageUtils.groovy
+++ src/org/sanwrit/tdect/io/ImageUtils.groovy
@@ -65,44 +65,6 @@ class ImageUtils {
     }
 
     /**
-     * Calculates the average of the red, green, and blue colors of a
-     * picture element.
-     *
-     * @param image
-     * @return BufferedImage
-     */
-    static def grayScaleAverage(def image) {
-        def rgbBuff = image.getRGB(0, 0, image.getWidth(),
-                image.getHeight(), null, 0, image.getWidth())
-        def tempBuff = new int[image.getHeight() * image.getWidth()]
-
-        rgbBuff.eachWithIndex { it, i ->
-            def pixel = new Color(it.next())
-            def alpha = pixel.getAlpha()
-            def red = pixel.getRed()
-            def green = pixel.getGreen()
-            def blue = pixel.getBlue()
-            def average = ((red + green + blue) / 3) as int //the average for the pixel
-            def color = new Color(average, average, average, alpha)
-            tempBuff[i] = color.getRGB()
-        }
-        return toImage(tempBuff, image.getWidth(), image.getHeight())
-    }
-
-    private def static int2byte(int[] src) {
-        byte[] dst = new byte[src.length * 4]
-        int j = 0
-        for (i in 0..src.length - 1) {
-            int a = src[i]
-            dst[j++] = (byte) ((a >> 24) & 0xFF)
-            dst[j++] = (byte) ((a >> 16) & 0xFF)
-            dst[j++] = (byte) ((a >> 8) & 0xFF)
-            dst[j++] = (byte) (a & 0xFF)
-        }
-        return dst
-    }
-
-    /**
      * Converts a byte array to BufferedImage
      * @param data
      * @return
--- test/org/sanwrit/tdect/io/ImageUtilsTest.groovy
+++ test/org/sanwrit/tdect/io/ImageUtilsTest.groovy
@@ -29,27 +29,6 @@ class ImageUtilsTest extends GroovyTestCase {
         assertTrue images.size() > 0
     }
 
-    void testGrayScaleAverage() {
-        String path = URLDecoder.decode(this.getClass().getResource("in").getPath()
-                + File.separator, "UTF-8")
-        File inputDir = new File(path)
-
-        assertTrue inputDir.isDirectory()
-
-        def images = []
-        for (file in inputDir.listFiles()) {
-            images.add(ImageUtils.readImage(file.getAbsoluteFile()
-                    .getAbsolutePath()))
-        }
-
-        assertTrue images.size() > 0
-
-        images.each { it ->
-            def img = ImageUtils.grayScaleAverage(it)
-            File newFile = new File(inputDir.getAbsolutePath() + "/temp.png")
-            assertTrue ImageUtils.writeImage(img, "PNG", newFile)
-        }
-    }
 }
 
 
